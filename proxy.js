dd2a49f31f46fdbd0ac9e46b155c1d3c21fba603
josecesfranjo <joseantonio.cesfranjo@room714.com>
2026-01-27 21:25:05 +0100

    upload images

--- proxy.js
+++ proxy.js
@@ -44,6 +44,6 @@ export default proxy;
 
 export const config = {
   matcher: [
-    "/((?!api/auth|_next/static|_next/image|favicon.ico|.*\\.svg$|.*\\.png$|.*\\.jpg$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.svg$|.*\\.png$|.*\\.jpg$).*)",
   ],
 };
