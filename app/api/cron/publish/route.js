63d435267f3782f8f403fab05603748ea45141cd
josecesfranjo <joseantonio.cesfranjo@room714.com>
2026-01-27 22:29:03 +0100

    again

--- app/api/cron/publish/route.js
+++ app/api/cron/publish/route.js
@@ -1,5 +1,5 @@
 import { prisma } from "@/app/lib/prisma";
-import { triggerLinkedInNotification } from "@/app/admin/components/actions";
+import { triggerLinkedInNotification } from "@/app/admin/actions";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
