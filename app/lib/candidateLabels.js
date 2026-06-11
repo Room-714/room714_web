834de8de1596daf55ee4163d7b72ae4f96e4a533
josecesfranjo <joseantonio.cesfranjo@room714.com>
2026-06-11 18:26:38 +0200

    chore(admin): etiquetas compartidas de posición y formación

---
+++ app/lib/candidateLabels.js
@@ -0,0 +1,12 @@
+export const POSITION_LABEL = {
+  DEVELOPER: "Desarrollador",
+  DESIGNER: "Diseñador",
+  PRODUCT_MANAGER: "Product Manager",
+};
+
+export const EDUCATION_LABEL = {
+  GRADO: "Grado Universitario",
+  MASTER: "Máster Universitario",
+  DOCTORADO: "Doctorado",
+  OTHER: "Otra",
+};
